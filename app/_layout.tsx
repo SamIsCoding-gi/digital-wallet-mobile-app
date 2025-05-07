a442414f83536ad1d2e99588f714b1980917abb1
Samuel Kibunda <samkaizi40@gmail.com>
2025-05-07 12:17:34 +0200

    Refactor code structure for improved readability and maintainability

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,16 +6,34 @@ import CreateAccount from "./signin-login/create-account";
 import Home from "./home/home";
 import Transfer from "./transfer/transfer";
 import Index from "./index";
+import * as SplashScreen from "expo-splash-screen";
+import { Text, View, Image } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 export default function RootLayout() {
   const [loggedin, setLoggedIn] = useState(false);
+  const [appIsReady, setAppIsReady] = useState(false);
 
   useEffect(() => {
-    getUser();
+    prepareApp();
   }, []);
 
+  const prepareApp = async () => {
+    try {
+      await SplashScreen.preventAutoHideAsync();
+      // Customize the splash screen delay or add additional logic here
+      await new Promise((resolve) => setTimeout(resolve, 3000)); // Example: 3-second delay
+      await getUser();
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      setAppIsReady(true);
+      await SplashScreen.hideAsync();
+    }
+  };
+
+  // checks if user is logged in
   const getUser = async () => {
     const user = await AsyncStorage.getItem("user");
     if (user) {
@@ -26,6 +44,43 @@ export default function RootLayout() {
       console.log("No user found");
     }
   };
+
+  if (!appIsReady) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#fff",
+        }}
+      >
+        <Image
+          source={require("../assets/images/logo.png")}
+          style={{ width: 100, height: 100, marginBottom: 20 }}
+        />
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "bold",
+            color: "#278727",
+            marginBottom: 20,
+          }}
+        >
+          Your Digital Wallet Patner!
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            color: "black",
+            marginBottom: 20,
+          }}
+        >
+          © Samuel Kibunda 2025
+        </Text>
+      </View>
+    );
+  }
 
   return (
     <Stack.Navigator
